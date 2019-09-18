23fdf4e5380d50b1ac2ca155ae677864da6234b3
Ruber Hernandez <ruberhenandez@gmail.com>
2019-09-17 20:22:51 -0500

    se agrega handleVisibilityChange()

--- javascript-profesional/assets/plugins/AutoPause.js
+++ javascript-profesional/assets/plugins/AutoPause.js
@@ -1,33 +1,46 @@
- class AutoPause {
-    constructor() {
-        this.threshold = 0.25;
-        this.handleIntersection = this.handleIntersection.bind(this);
+class AutoPause {
+  constructor() {
+      this.threshold = 0.25;
+      this.handleIntersection = this.handleIntersection.bind(this);
+      this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
+  }
+
+  run(player) {
+  
+      this.player = player;
+
+        const observer = new IntersectionObserver(this.handleIntersection, {
+            threshold: this.threshold,
+        });
+
+        observer.observe(this.player.media);
+
+        document.addEventListener('visibilitychange', this.handleVisibilityChange);
     }
 
-    run(player) {
-    
-        this.player = player;
+    handleIntersection(entries) {
+        const entry = entries[0];
 
-         const observer = new IntersectionObserver(this.handleIntersection, {
-             threshold: this.threshold,
-         });
+        const isVisible = entry.intersectionRatio >=  this.threshold;
 
-         observer.observe(this.player.media);
-     }
+        if (isVisible) {
+          this.player.play();
+        } else {
+          this.player.pause();
+        }
 
-     handleIntersection(entries) {
-         const entry = entries[0];
+      console.log(entry.intersectionRatio);
+    }
 
-         const isVisible = entry.intersectionRatio >=  this.threshold;
-
-         if (isVisible) {
-            this.player.play();
-          } else {
-            this.player.pause();
-          }
-
-        console.log(entry.intersectionRatio);
-     }
+    handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible';    
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+    console.log(document.visibilityState);
+    }
 
 
 }

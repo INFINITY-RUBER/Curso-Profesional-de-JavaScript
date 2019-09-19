dcf02258a76027ddec3d6ab64bcb66ea81b6c871
Ruber Hernandez <ruberhenandez@gmail.com>
2019-09-18 19:32:04 -0500

    se termina los ejercicios de typeScript

--- javascript-profesional/ejercicios/typescript/index.ts
+++ javascript-profesional/ejercicios/typescript/index.ts
@@ -39,3 +39,68 @@ comodin = { type: 'Wildcard' };
 
 // Object
 let someObject: object = { type: 'Wildcard' };
+// *******************FUNCIONES*****************************
+console.log(`*******************FUNCIONES*****************************`)
+// Funciones
+function add(a: number, b: number): number {
+    return a + b;
+}
+const sum = add(4, 6);
+
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {  //lastName: string lo define como SMITH POR OMISION si no e dado
+  return `${firstName} ${lastName}`;
+}
+
+function fullName2(firstName: string, lastName?: string ): string {  //lastName: string  es opcional
+    return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Agente');
+const richard2 = fullName2('Agente');
+
+console.log(richard);
+console.log(richard2);
+
+
+// ******************INTERFACES*****************************
+console.log(`*******************INTERFACES*****************************`)
+
+enum Color {
+Rojo = 'Rojo',
+Verde = 'Verde',
+}
+
+interface Rectangulo {
+ancho: number;
+alto: number;
+color?: Color;
+}
+
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    // color: Color.Rojo,
+};
+
+function area(r: Rectangulo): number {
+return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function() {
+    return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo sin parametro color `;
+};
+
+
+console.log(rect.toString());
